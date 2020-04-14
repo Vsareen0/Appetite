c979a6acf18243268a563e96b221caa40d0fc6a3
vsareen0 <vsareen24@gmail.com>
2020-04-14 17:07:11 +0530

    Nextjs Link added and navbar setup

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,5 +1,7 @@
-import { useState } from 'react';
-import { APP_NAME } from '../config';
+import { useState } from "react";
+import { APP_NAME } from "../config";
+import Link from 'next/link';
+
 import {
   Collapse,
   Navbar,
@@ -12,8 +14,8 @@ import {
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
-  NavbarText
-} from 'reactstrap';
+  NavbarText,
+} from "reactstrap";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -23,22 +25,27 @@ const Header = () => {
   return (
     <div>
       <Navbar color="light" light expand="md">
-  <NavbarBrand href="/">{APP_NAME}</NavbarBrand>
+        <Link href="/">
+        <NavLink className="font-weight-bold">{APP_NAME}</NavLink>
+        </Link>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="ml-auto" navbar>
             <NavItem>
-              <NavLink href="/components/">Components</NavLink>
+              <Link href="/signin">
+                <NavLink>Signin</NavLink>
+              </Link>
             </NavItem>
             <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+              <Link href="/signup">
+                <NavLink>Signup</NavLink>
+              </Link>
             </NavItem>
           </Nav>
-          <NavbarText>Simple Text</NavbarText>
         </Collapse>
       </Navbar>
     </div>
   );
-}
+};
 
 export default Header;
---
+++ frontend/pages/signin.js
@@ -0,0 +1,15 @@
+import Layout from '../components/Layout';
+import Link from 'next/link';
+
+const Signin = () => {
+    return (
+        <Layout>
+            <h2>Signin page</h2>
+            <Link href="/">
+                <a>Home</a>
+            </Link>
+        </Layout>
+    )
+};
+
+export default Signin;
