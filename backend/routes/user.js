958bf0489f5abcd7fab8f23bc4218bf6741a2975
vsareen0 <vsareen24@gmail.com>
2020-04-20 21:29:14 +0530

    Added user route and controller

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { requireSignIn, authMiddleware } = require('../controllers/auth');
-const { read } = require('../controllers/user');
+const { read, publicProfile } = require('../controllers/user');
 
 router.get("/profile", requireSignIn, authMiddleware, read ); 
+router.get("/user/:username", publicProfile ); 
 
 
 module.exports = router;
